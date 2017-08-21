3378c9cdaf9eb91990baedfbaf05987779745135
SDGoodwin <seangoodwin75@gmail.com>
2017-08-21 12:59:23 -0400

    18aug

--- Windows.ps1
+++ Windows.ps1
@@ -1,3 +1,18 @@
 # This is a work in progress. Please feel free to contribute at https://github.com/SDGoodwin/PCI_Scripts with either PR or Issues.
 #
 # This is being written for PCI DSS v3.2
+
+# Timestamp data collection
+echo %date% %time% >> 
+
+#System Overview
+systeminfo >>
+
+#Group Policy Results (Super-Verbose)
+gpresult /z >> 
+
+# Active connections
+netstat -an >>  
+
+# Active Routes
+netstat -r
