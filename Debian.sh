204707c1eca1649e6b0c23774e075d3fe34171f2
SDGoodwin <seangoodwin75@gmail.com>
2017-09-01 17:10:06 -0400

    slow work

--- Debian.sh
+++ Debian.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This is a work in progress. Please feel free to contribute at https://github.com/SDGoodwin/PCI_Scripts with either PR or Issues.
+# Credit to https://github.com/jbarcia/PCI-Audit-Script for starting points
 #
 # This is being written for PCI DSS v3.2
 # Some of these commands will require elevated privleges 
@@ -26,20 +27,38 @@ fi
 mkdir "$tempdir"
 
 
+# General Info
+whoami >> "$tempdir/general_info.log"
+echo "********************************" >> "$tempdir/general_info.log"
+echo "********************************" >> "$tempdir/general_info.log"
+uname -a >> "$tempdir/general_info.log"
+echo "********************************" >> "$tempdir/general_info.log"
+echo "********************************" >> "$tempdir/general_info.log"
+uptime >> "$tempdir/general_info.log"
+echo "********************************" >> "$tempdir/general_info.log"
+echo "********************************" >> "$tempdir/general_info.log"
+timedatectl >> "$tempdir/general_info.log"
+echo "********************************" >> "$tempdir/general_info.log"
+echo "********************************" >> "$tempdir/general_info.log"
+mount >> "$tempdir/general_info.log"
+
+
 # Grab NTP Configurations for Req: 10.4, 10.4.1, 10.4.2. 10.4.3
 cat /etc/ntp.conf >> "$tempdir/ntp_config.log"
 cat /etc/xntp.conf >> "$tempdir/xntp_config.log"
 cat /var/lib/ntp/ntp.conf.dhcp >> "$tempdir/dhcp_ntp.log"
 
-# Grab a list of all local users
+# Grab a list of all local users for Req 2.1
 cat  /etc/passwd >> "$tempdir/passwd.log"
-echo ******************************** >>"$tempdir/passwd.log"
-echo ******************************** >>"$tempdir/passwd.log"
+echo "********************************" >>"$tempdir/passwd.log"
+echo "********************************" >>"$tempdir/passwd.log"
 getent passwd >>"$tempdir/passwd.log"
 cat /etc/shadow >> "$tempdir/shadow.log"
 
+
 # List users with rights to sudo
 getent group sudo | cut -d: -f4 >> "$tempdir/sudoers.log"
+
 
 # Grab the Login.defs file
 cat /etc/login.defs >> "$tempdir/login_defs.log"
@@ -49,18 +68,20 @@ cat /etc/login.defs >> "$tempdir/login_defs.log"
 dpkg-query -l >> "$tempdir/all_packages.log"
 
 
-# Services and status codes
+# Services and status codes for Req 2.2.2
 service --status-all >> "$tempdir/service_status.log"
-
 ps aux >> "$tempdir/ps_aux.log"
 ps -ef >> "$tempdir/ps_ef.log"
 cat /etc/services >> "$tempdir/services.log"
 
+
 # List Cron job files
 ls /etc/cron.* >> "$tempdir/cron_files.log"
 
+
 # Network Configurations
 ifconfig >> "$tempdir/ifconfig.log"
+
 
 # Local Firewall config
 iptables -L -v -n --line-numbers >> "$tempdir/iptables.log"
@@ -68,32 +89,41 @@ iptables -L -v -n --line-numbers >> "$tempdir/iptables.log"
 # Rsyslog.conf
 cat /etc/rsyslog.conf >> "$tempdir/Rsyslog_conf.log"
 
+
 # Rsyslog.d Contents
 ls /etc/rsyslog.d/ >> "$tempdir/Rsyslog_d_contents.log"
 cat /etc/rsyslog.d/* >> "$tempdir/Rsyslog_d_contents_detail.log"
 
+
 # Distro Info
 cat /etc/issue >> "$tempdir/Distro_Info.log"
-echo ******************************** >> "$tempdir/Distro_Info.log"
-echo ******************************** >> "$tempdir/Distro_Info.log"
+echo "********************************" >> "$tempdir/Distro_Info.log"
+echo "********************************" >> "$tempdir/Distro_Info.log"
 cat /etc/*-release >> "$tempdir/Distro_Info.log"
-echo ******************************** >> "$tempdir/Distro_Info.log"
-echo ******************************** >> "$tempdir/Distro_Info.log"
+echo "********************************" >> "$tempdir/Distro_Info.log"
+echo "********************************" >> "$tempdir/Distro_Info.log"
 cat /etc/lsb-release >> "$tempdir/Distro_Info.log"
+
 
 # Kernel info
 cat /proc/version >> "$tempdir/kernel.log"
-echo ******************************** >> "$tempdir/kernel.log"
-echo ******************************** >> "$tempdir/kernel.log"
+echo "********************************" >> "$tempdir/kernel.log"
+echo "********************************" >> "$tempdir/kernel.log"
 uname -a >> "$tempdir/kernel.log"
-echo ******************************** >> "$tempdir/kernel.log"
-echo ******************************** >> "$tempdir/kernel.log"
+echo "********************************" >> "$tempdir/kernel.log"
+echo "********************************" >> "$tempdir/kernel.log"
 uname -mrs >> "$tempdir/kernel.log"
-echo ******************************** >> "$tempdir/kernel.log"
-echo ******************************** >> "$tempdir/kernel.log"
+echo "********************************" >> "$tempdir/kernel.log"
+echo "********************************" >> "$tempdir/kernel.log"
 dmesg | grep Linux >> "$tempdir/kernel.log"
-echo ******************************** >> "$tempdir/kernel.log"
-echo ******************************** >> "$tempdir/kernel.log"
+echo "********************************" >> "$tempdir/kernel.log"
+echo "********************************" >> "$tempdir/kernel.log"
 ls /boot | grep vmlinuz >> "$tempdir/kernel.log"
+
+# Check for RK Hunter
+rkhunter --versioncheck >> "$tempdir/RKHunter.log"
+
+# Check for CHK Rootkit
+chkrootkit -V >> "$tempdir/CHKRootkit.log"
 
 exit 
--- Windows.ps1
+++ Windows.ps1
@@ -1,6 +1,7 @@
 # This is a work in progress. Please feel free to contribute at https://github.com/SDGoodwin/PCI_Scripts with either PR or Issues.
 #
 # This is being written for PCI DSS v3.2
+# Credit to https://github.com/jbarcia/PCI-Audit-Script
 
 # Timestamp data collection
 echo %date% %time% >> 
